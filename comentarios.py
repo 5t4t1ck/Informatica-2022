3dcf2adddd6a8cf9b534de07126dc5b7893e15e6
Diego Saavedra <dsaavedra88@gmail.com>
2022-08-09 21:44:35 +0200

    Comentarios

---
+++ comentarios.py
@@ -0,0 +1,14 @@
+# Esto es un comentario
+
+"""
+Esto es
+un comentario
+de más de una linea
+"""
+
+a = 2 
+b = 4 # Esto es una suma de 2 numeros
+
+resultado = a + b
+
+print("La suma es:", resultado)
