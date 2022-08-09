7d9ea2e4ca54da8a644187a8d2fd03759140c2c6
Diego Saavedra <dsaavedra88@gmail.com>
2022-08-09 21:59:46 +0200

    variables

---
+++ variables.py
@@ -0,0 +1,31 @@
+nombrePersona = "Diego Saavedra"
+print(nombrePersona)
+print(type(nombrePersona))
+
+numero = 5
+print(numero)
+print(type(numero))
+
+nacido_vivo = True
+print(nacido_vivo)
+print(type(nacido_vivo))
+
+nota = 9.5
+print(nota)
+print(type(nota))
+
+complejo = 1j
+print(complejo)
+print(type(complejo))
+
+nombre, apellido, telefono = "Diego", "Saavedra", "0944521236"
+print(nombre, apellido, telefono)
+
+numero_inicial = numero_medio = numero_final = 10
+print(numero_inicial, numero_medio, numero_final)
+
+nombre = "Diego"
+apellido = "Saavedra"
+#print(nombre + apellido)
+#print(nombre +" "+ apellido)
+print(nombre, apellido)
